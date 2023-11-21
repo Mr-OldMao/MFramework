a21b2733d84fc5d32b74db616bbb1ef7e7634e03
Mr_Mao <929764654@qq.com>
2023-11-21 16:41:19 +0800

    自动打包 webgl端打完完毕后自动打开文件夹

--- MFramework/Framework/4Editor/BuildTool/BuildTool.cs
+++ MFramework/Framework/4Editor/BuildTool/BuildTool.cs
@@ -7,192 +7,199 @@ using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
 using UnityEngine;
 
-/// <summary>
-/// 标题：自动打包工具
-/// 作者：毛俊峰
-/// 时间：2023-04-18
-/// 功能：根据参数配置一键打包当前平台(Android,PC)的项目,
-/// 编辑器菜单：一键打包 快捷键Shift+B  MFreamwork/BuildTool/BuildTargetPlatformBundle  
-///             参数配置 快捷键Shift+S  MFreamwork/BuildTool/BuildConfigSetting
-/// </summary>
-public class BuildTool : IPreprocessBuildWithReport, IPostprocessBuildWithReport
+namespace MFramework
 {
-    public const string EditorMenuRootPath = "MFramework/BuildTool";
-
-    public int callbackOrder => 0;
-    public void OnPreprocessBuild(BuildReport report)
-    {
-        Debug.Log("Building Start...  StartTime:" + report.summary.buildStartedAt);
-    }
-
-    public void OnPostprocessBuild(BuildReport report)
-    {
-        Debug.Log("Building End" +
-            "\nOutPutPath：" + report.summary.outputPath + "，" +
-            "\nSize:" + string.Format("{0:N1}", report.summary.totalSize / 1024f / 1024f) + "M，" +
-            "\nEndTime:" + report.summary.buildEndedAt
-            );
-    }
-
-
-    [MenuItem((EditorMenuRootPath + "/BuildTargetPlatformBundle #B"))]
-    public static void EditorBuildCurrentPlatform()
-    {
-        Build(PlayerPrefs.GetString(PlayerPrefsKeyGroup.Build_OutPutPath_Editor, BuildConfigSetting.BuildRootDefaultPath));
-    }
     /// <summary>
-    /// 当前打包的导出的全路径
+    /// 标题：自动打包工具
+    /// 作者：毛俊峰
+    /// 时间：2023-04-18
+    /// 功能：根据参数配置一键打包当前平台(Android,PC)的项目,
+    /// 编辑器菜单：一键打包 快捷键Shift+B  MFreamwork/BuildTool/BuildTargetPlatformBundle  
+    ///             参数配置 快捷键Shift+S  MFreamwork/BuildTool/BuildConfigSetting
     /// </summary>
-    static string curBuildOutPutAllPath;
-    /// <summary>
-    /// 产品名称
-    /// </summary>
-    static string appName;
-    /// <summary>
-    /// Windows全路径 打包完成自动打开文件夹
-    /// </summary>
-    static string buildEndAutoOpenFolderPath;
-
-
-    /// <summary>
-    /// 自动根据当前平台打包打包
-    /// </summary>
-    /// <param name="outPutFolderPath">导出包体的根目录</param>
-    private static void Build(string outPutFolderPath)
+    public class BuildTool : IPreprocessBuildWithReport, IPostprocessBuildWithReport
     {
-        if (string.IsNullOrEmpty(outPutFolderPath))
+        public const string EditorMenuRootPath = "MFramework/BuildTool";
+
+        public int callbackOrder => 0;
+        public void OnPreprocessBuild(BuildReport report)
         {
-            Debug.LogError("打包失败！打包路径为空，请使用编辑器工具配置打包路径 " + EditorMenuRootPath + "/BuildConfigSetting");
-            return;
+            Debugger.Log("Building Start...  StartTime:" + report.summary.buildStartedAt, LogTag.MF);
         }
-        string curTime = string.Format("{0:yyyy_MM_dd_HH_mm_ss}", DateTime.Now);
-        appName = PlayerSettings.productName;
-        curBuildOutPutAllPath = outPutFolderPath + "/" + appName + "_v" + PlayerSettings.bundleVersion + "_" + curTime;
-        buildEndAutoOpenFolderPath = string.Empty;
-        if (EditorUserBuildSettings.activeBuildTarget == BuildTarget.Android)
-        {
-            //代码配置秘钥
-            //PlayerSettings.Android.keystorePass = "sikiocean";
-            //PlayerSettings.Android.keyaliasPass = "sikiocean";
-            //PlayerSettings.Android.keyaliasName = "android.keystore";
-            //PlayerSettings.Android.keystoreName = Application.dataPath.Replace("/Assets", "") + "/realfram.keystore";
-            curBuildOutPutAllPath += ".apk";
-            buildEndAutoOpenFolderPath = outPutFolderPath;
-        }
-        else if (EditorUserBuildSettings.activeBuildTarget == BuildTarget.iOS)
-        {
 
-        }
-        else if (EditorUserBuildSettings.activeBuildTarget == BuildTarget.StandaloneWindows || EditorUserBuildSettings.activeBuildTarget == BuildTarget.StandaloneWindows64)
+        public void OnPostprocessBuild(BuildReport report)
         {
-            buildEndAutoOpenFolderPath = curBuildOutPutAllPath;
-            curBuildOutPutAllPath += "/" + appName + ".exe";
+            Debugger.Log("Building End" +
+                "\nOutPutPath：" + report.summary.outputPath + "，" +
+                "\nSize:" + string.Format("{0:N1}", report.summary.totalSize / 1024f / 1024f) + "M，" +
+                "\nEndTime:" + report.summary.buildEndedAt
+                , LogTag.MF);
         }
-        BuildPipeline.BuildPlayer(FindEnableEditorrScenes(), curBuildOutPutAllPath, EditorUserBuildSettings.activeBuildTarget, BuildOptions.None);
-        OpenDirectory(buildEndAutoOpenFolderPath);
-    }
 
-    #region 自动化打包基于jenkins或windows command批处理命令
 
-    /// <summary>
-    /// 打包PC端
-    /// 自动化打包基于jenkins或windows command批处理命令
-    /// </summary>
-    /// .bat命令
-    //:: 此处执行Unity打包
-    //echo 正在打包中请稍后...
-    //"D:\UnityEngine\2021.3.20f1c1\Editor\Unity.exe" ^
-    //-quit ^
-    //-batchmode ^
-    //-projectPath "D:\LearnPath\AutoBuildByJenkins\LearnJenkins" ^
-    //-executeMethod BuildTool.BuildByCMD ^
-    //-logFile "D:\AutoBuildlog.txt" ^
-    //echo 打包完成
-    //exit
-    public static void BuildByCMD()
-    {
-        Debug.Log("Building......");
-        SetConfigSetting();
-        Build(PlayerPrefs.GetString(PlayerPrefsKeyGroup.Build_OutPutPath_CMD, BuildConfigSetting.BuildRootDefaultPath));
-    }
-    #endregion
-    /// <summary>
-    /// 设置打包参数
-    /// 自动化打包基于jenkins或windows command批处理命令
-    /// </summary>
-    public static void SetConfigSetting()
-    {
-        // 解析命令行参数
-        string[] args = System.Environment.GetCommandLineArgs();
-        Debug.Log("------------------------------------------------------------解析CMD参数------------------------------------------------------------");
-        foreach (var s in args)
+        [MenuItem((EditorMenuRootPath + "/BuildTargetPlatformBundle #B"))]
+        public static void EditorBuildCurrentPlatform()
         {
-            Debug.Log("参数遍历 arg:" + s);
-            if (s.Contains("--productName:"))
+            Build(PlayerPrefs.GetString(PlayerPrefsKeyGroup.Build_OutPutPath_Editor, BuildConfigSetting.BuildRootDefaultPath));
+        }
+        /// <summary>
+        /// 当前打包的导出的全路径
+        /// </summary>
+        static string curBuildOutPutAllPath;
+        /// <summary>
+        /// 产品名称
+        /// </summary>
+        static string appName;
+        /// <summary>
+        /// Windows全路径 打包完成自动打开文件夹
+        /// </summary>
+        static string buildEndAutoOpenFolderPath;
+
+
+        /// <summary>
+        /// 自动根据当前平台打包打包
+        /// </summary>
+        /// <param name="outPutFolderPath">导出包体的根目录</param>
+        private static void Build(string outPutFolderPath)
+        {
+            if (string.IsNullOrEmpty(outPutFolderPath))
             {
-                Debug.Log("--productName:" + s);
-                string productName = s.Split(':')[1];
-                // 设置app名字
-                PlayerSettings.productName = productName;
+                Debugger.LogError("打包失败！打包路径为空，请使用编辑器工具配置打包路径 " + EditorMenuRootPath + "/BuildConfigSetting");
+                return;
             }
-            if (s.Contains("--version:"))
+            string curTime = string.Format("{0:yyyy_MM_dd_HH_mm_ss}", DateTime.Now);
+            appName = PlayerSettings.productName;
+            curBuildOutPutAllPath = outPutFolderPath + "/" + appName + "_v" + PlayerSettings.bundleVersion + "_" + curTime;
+            buildEndAutoOpenFolderPath = string.Empty;
+            if (EditorUserBuildSettings.activeBuildTarget == BuildTarget.Android)
             {
-                Debug.Log("--version:" + s);
-                string version = s.Split(':')[1];
-                // 设置版本号
-                PlayerSettings.bundleVersion = version;
-                foreach (var item in s.Split(':'))
+                //代码配置秘钥
+                //PlayerSettings.Android.keystorePass = "sikiocean";
+                //PlayerSettings.Android.keyaliasPass = "sikiocean";
+                //PlayerSettings.Android.keyaliasName = "android.keystore";
+                //PlayerSettings.Android.keystoreName = Application.dataPath.Replace("/Assets", "") + "/realfram.keystore";
+                curBuildOutPutAllPath += ".apk";
+                buildEndAutoOpenFolderPath = outPutFolderPath;
+            }
+            else if (EditorUserBuildSettings.activeBuildTarget == BuildTarget.iOS)
+            {
+
+            }
+            else if (EditorUserBuildSettings.activeBuildTarget == BuildTarget.StandaloneWindows || EditorUserBuildSettings.activeBuildTarget == BuildTarget.StandaloneWindows64)
+            {
+                buildEndAutoOpenFolderPath = curBuildOutPutAllPath;
+                curBuildOutPutAllPath += "/" + appName + ".exe";
+            }
+            else if (EditorUserBuildSettings.activeBuildTarget == BuildTarget.WebGL)
+            {
+                buildEndAutoOpenFolderPath = curBuildOutPutAllPath;
+            }
+            BuildPipeline.BuildPlayer(FindEnableEditorrScenes(), curBuildOutPutAllPath, EditorUserBuildSettings.activeBuildTarget, BuildOptions.None);
+            OpenDirectory(buildEndAutoOpenFolderPath);
+        }
+
+        #region 自动化打包基于jenkins或windows command批处理命令
+
+        /// <summary>
+        /// 打包PC端
+        /// 自动化打包基于jenkins或windows command批处理命令
+        /// </summary>
+        /// .bat命令
+        //:: 此处执行Unity打包
+        //echo 正在打包中请稍后...
+        //"D:\UnityEngine\2021.3.20f1c1\Editor\Unity.exe" ^
+        //-quit ^
+        //-batchmode ^
+        //-projectPath "D:\LearnPath\AutoBuildByJenkins\LearnJenkins" ^
+        //-executeMethod BuildTool.BuildByCMD ^
+        //-logFile "D:\AutoBuildlog.txt" ^
+        //echo 打包完成
+        //exit
+        public static void BuildByCMD()
+        {
+            Debugger.Log("Building......", LogTag.MF);
+            SetConfigSetting();
+            Build(PlayerPrefs.GetString(PlayerPrefsKeyGroup.Build_OutPutPath_CMD, BuildConfigSetting.BuildRootDefaultPath));
+        }
+        #endregion
+        /// <summary>
+        /// 设置打包参数
+        /// 自动化打包基于jenkins或windows command批处理命令
+        /// </summary>
+        public static void SetConfigSetting()
+        {
+            // 解析命令行参数
+            string[] args = System.Environment.GetCommandLineArgs();
+            Debugger.Log("------------------------------------------------------------解析CMD参数------------------------------------------------------------", LogTag.MF);
+            foreach (var s in args)
+            {
+                Debugger.Log("参数遍历 arg:" + s, LogTag.MF);
+                if (s.Contains("--productName:"))
                 {
-                    Debug.Log("BBB " + item);
+                    Debugger.Log("--productName:" + s, LogTag.MF);
+                    string productName = s.Split(':')[1];
+                    // 设置app名字
+                    PlayerSettings.productName = productName;
+                }
+                if (s.Contains("--version:"))
+                {
+                    Debugger.Log("--version:" + s, LogTag.MF);
+                    string version = s.Split(':')[1];
+                    // 设置版本号
+                    PlayerSettings.bundleVersion = version;
+                    foreach (var item in s.Split(':'))
+                    {
+                        Debugger.Log("BBB " + item, LogTag.MF);
+                    }
+                }
+                if (s.Contains("--outPutPath:"))
+                {
+                    Debugger.Log("--outPutPath:" + s, LogTag.MF);
+                    string buildPath = s.Split(':')[1] + ":" + s.Split(':')[2];
+                    PlayerPrefs.SetString(PlayerPrefsKeyGroup.Build_OutPutPath_CMD, buildPath);
                 }
             }
-            if (s.Contains("--outPutPath:"))
+        }
+
+        #region command批处理命令
+
+        #endregion
+
+        #region Other
+        /// <summary>
+        /// 获得在BuildSettings已经添加的场景
+        /// </summary>
+        /// <returns></returns>
+        private static string[] FindEnableEditorrScenes()
+        {
+            List<string> editorScenes = new List<string>();
+            foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
             {
-                Debug.Log("--outPutPath:" + s);
-                string buildPath = s.Split(':')[1] + ":" + s.Split(':')[2];
-                PlayerPrefs.SetString(PlayerPrefsKeyGroup.Build_OutPutPath_CMD, buildPath);
+                if (!scene.enabled) continue;
+                editorScenes.Add(scene.path);
+
+                Debugger.Log(scene.path, LogTag.MF);
             }
+            return editorScenes.ToArray();
         }
-    }
-
-    #region command批处理命令
-
-    #endregion
-
-    #region Other
-    /// <summary>
-    /// 获得在BuildSettings已经添加的场景
-    /// </summary>
-    /// <returns></returns>
-    private static string[] FindEnableEditorrScenes()
-    {
-        List<string> editorScenes = new List<string>();
-        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        /// <summary>
+        /// 打开Windows指定文件夹
+        /// </summary>
+        /// <param name="path"></param>
+        public static void OpenDirectory(string path)
         {
-            if (!scene.enabled) continue;
-            editorScenes.Add(scene.path);
+            if (string.IsNullOrEmpty(path)) return;
 
-            Debug.Log(scene.path);
+            path = path.Replace("/", "\\");
+            if (!Directory.Exists(path))
+            {
+                Debugger.LogError("No Directory: " + path);
+                return;
+            }
+            System.Diagnostics.Process.Start("explorer.exe", path);
         }
-        return editorScenes.ToArray();
-    }
-    /// <summary>
-    /// 打开Windows指定文件夹
-    /// </summary>
-    /// <param name="path"></param>
-    public static void OpenDirectory(string path)
-    {
-        if (string.IsNullOrEmpty(path)) return;
 
-        path = path.Replace("/", "\\");
-        if (!Directory.Exists(path))
-        {
-            Debug.LogError("No Directory: " + path);
-            return;
-        }
-        System.Diagnostics.Process.Start("explorer.exe", path);
+        #endregion
     }
-
-    #endregion
 }
 #endif
